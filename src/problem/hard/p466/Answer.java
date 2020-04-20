d69b65351f048c5ee6922cc0d835fc80398fe5da
caoPhoenix <caophoenix@163.com>
2020-04-20 12:15:13 +0800

    today

--- src/problem/hard/p466/Answer.java
+++ src/problem/hard/p466/Answer.java
@@ -1,4 +1,118 @@
 package problem.hard.p466;
 
 public class Answer {
+    public static void main(String[] args) {
+        System.out.println(new Solution().getMaxRepetitions(
+                "phqghumeaylnlfdxfircvscxggbwkfnqduxwfnfozvsrtkjprepggxrpnrvystmwcysyycqpevikef",
+                1000000,
+                "fmznimkkasvwsrenzkycxfxtlsgypsfad",
+                333
+        ));
+    }
+}
+
+/**
+ * @author caoPhoenix
+ * @date 2020/04/19
+ *
+ * 在 _1 的基础上改过来，这次超时了
+ * 第31/41个通过测试用例
+ * "phqghumeaylnlfdxfircvscxggbwkfnqduxwfnfozvsrtkjprepggxrpnrvystmwcysyycqpevikef"
+ * 1000000
+ * "fmznimkkasvwsrenzkycxfxtlsgypsfad"
+ * 333
+ *
+ * 结果是333是对的，527ms也超时，我不玩了
+ */
+class Solution {
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        char[] chs1 = s1.toCharArray();
+        char[] chs2 = s2.toCharArray();
+        int len_S1 = chs1.length * n1;
+        int len_S2 = chs2.length * n2;
+        // S1 -> S2 M
+        int M = 0;
+        int point1 = 0;
+        boolean hit = false;
+        while (point1 < len_S1) {
+            for (int i = 0; i < len_S2; i++) {
+                hit = false;
+                for (; point1 < len_S1; point1++) {
+                    if (chs1[point1 % chs1.length] == chs2[i % chs2.length]) {
+                        hit = true;
+                        break;
+                    }
+                }
+                // S1扫完都没匹配，说明不能获得
+                if (!hit) {
+                    // System.out.println("不匹配");
+                    return M;
+                }
+                // 此时hit = true. 若S1扫完时，S2都还没走到尾，说明也不能转换
+                if (point1 == len_S1 - 1 && i < len_S2 - 1) {
+                    // System.out.println("S1扫完了" + i);
+                    return M;
+                }
+                point1++;
+            }
+            // 到这里，S2 能够扫描完，说明S1可以获得S2，开始计算M
+            // S1 [0, point1] 可以得到 S2
+            M++;
+        }
+        return M;
+    }
+}
+
+/**
+ * @author caoPhoenix
+ * @date 2020/04/19
+ *
+ * 直接拼字符串会超出内存限制
+ */
+class Solution_2 {
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        // 转换成 char[] 访问快
+        char[] string1 = s1.toCharArray();
+        char[] string2 = s2.toCharArray();
+        // 字符串 S1 S2
+        char[] chs1 = new char[string1.length * n1];
+        char[] chs2 = new char[string2.length * n2];
+        for (int i = 0; i < chs1.length; i++) {
+            chs1[i] = string1[i % string1.length];
+        }
+        for (int i = 0; i < chs2.length; i++) {
+            chs2[i] = string2[i % string2.length];
+        }
+
+        // S1 -> S2 M
+        int M = 0;
+        int point1 = 0;
+        boolean hit = false;
+        while (point1 < chs1.length) {
+            for (int i = 0; i < chs2.length; i++) {
+                hit = false;
+                for (; point1 < chs1.length; point1++) {
+                    if (chs1[point1] == chs2[i]) {
+                        hit = true;
+                        break;
+                    }
+                }
+                // S1扫完都没匹配，说明不能获得
+                if (!hit) {
+                    System.out.println("不匹配");
+                    return M;
+                }
+                // 此时hit = true. 若S1扫完时，S2都还没走到尾，说明也不能转换
+                if (point1 == chs1.length - 1 && i < chs2.length - 1) {
+                    System.out.println("S1扫完了" + i);
+                    return M;
+                }
+                point1++;
+            }
+            // 到这里，S2 能够扫描完，说明S1可以获得S2，开始计算M
+            // S1 [0, point1] 可以得到 S2
+            M++;
+        }
+        return M;
+    }
 }
--- src/problem/medium/p200/Answer.java
+++ src/problem/medium/p200/Answer.java
@@ -1,4 +1,110 @@
 package problem.medium.p200;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
 public class Answer {
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+        System.out.println(new Solution_2().numIslands(grid));
+    }
+}
+
+/**
+ * @author caoPhoenix
+ * @date 2020/04/20
+ * time 6ms 23%
+ * mem 42.1MB 6%
+ * BFS
+ */
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int height = grid.length;
+        int width = grid[0].length;
+        int count = 0;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] marked = new boolean[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (!marked[i][j] && grid[i][j] == '1') {
+                    marked[i][j] = true;
+                    queue.add(new int[]{i, j});
+                    count++;    // 发现一块独立大陆
+                }
+                while (!queue.isEmpty()) {
+                    // 把该大陆四邻域的全部标记
+                    int[] now = queue.remove();
+                    for (int k = 0; k < 4; k++) {
+                        int tx = now[0] + dx[k];
+                        int ty = now[1] + dy[k];
+                        if (tx >= 0 && tx < height && ty >= 0 && ty < width
+                                && !marked[tx][ty] && grid[tx][ty] == '1') {
+                            marked[tx][ty] = true;
+                            queue.add(new int[]{tx, ty});
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
+
+/**
+ * @author caoPhoenix
+ * @date 2020/04/20
+ * time 2ms 96.16%
+ * mem 42.2MB 6.25%
+ * DFS 比 BFS 快不少。。
+ */
+class Solution_2 {
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    int height;
+    int width;
+    int count = 0;
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return count;
+        }
+        height = grid.length;
+        width = grid[0].length;
+        boolean[][] marked = new boolean[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (!marked[i][j] && grid[i][j] == '1') {
+                    System.out.println(i + ", " + j);
+                    dfs(i, j, grid, marked);
+                    count++;
+                }
+
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int x, int y, char[][] grid, boolean[][] marked) {
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (tx >= 0 && tx < height && ty >= 0 && ty < width
+                    && !marked[tx][ty] && grid[tx][ty] == '1') {
+                marked[tx][ty] = true;
+                dfs(tx, ty, grid, marked);
+            }
+        }
+    }
 }
