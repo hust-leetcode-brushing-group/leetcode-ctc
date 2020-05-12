83199f5278ec65485b2e907bee4e39a0f5cb004a
caoPhoenix <caophoenix@163.com>
2020-05-12 17:30:14 +0800

    today

--- src/problem/easy/p155/Answer.java
+++ src/problem/easy/p155/Answer.java
@@ -11,10 +11,18 @@ public class Answer {
 
 
 /**
- * 解法一：
- * @Author:caoPhoenix
- * @Date:2019/07/10
- * @86.41%
+ * @author caoPhoenix
+ * @date 2019/07/10
+ * time 86.41%
+ *
+ * 更新：
+ * @date 2020/05/12
+ * 执行用时 :7 ms, 在所有 Java 提交中击败了85.70% 的用户
+ * 内存消耗 :43.5 MB, 在所有 Java 提交中击败了10.84%的用户
+ *
+ * 之前的代码现在再看只战胜7.64%了。
+ * 遂调把 capital 调成10倍，100000。
+ * 顺便感叹下要是我现在写，肯定会用库里的栈，而不是自己实现一个栈
  */
 class MinStack {
     private int[] data;
