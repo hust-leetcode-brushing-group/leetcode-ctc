291724af31f39ddbdd5dbec3c0a694fc91a060cc
caoPhoenix <caophoenix@163.com>
2020-05-16 21:28:43 +0800

    today

---
+++ src/problem/hard/p1095/Answer.java
@@ -0,0 +1,88 @@
+package problem.hard.p1095;
+
+/**
+ * 1095. 山脉数组中查找目标值
+ * https://leetcode-cn.com/problems/find-in-mountain-array/
+ */
+public class Answer {
+}
+
+
+interface MountainArray {
+    public int get(int index);
+    public int length();
+}
+
+/**
+ * @author caoPhoenix
+ * @date 2020/04/29
+ *
+ * 二分查找最佳实践
+ * 执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗 : 39.5 MB , 在所有 Java 提交中击败了 100.00% 的用户
+ */
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int max = mountainArr.length() - 1;
+
+        // 先二分找出peak
+        int peak = findPeak(0, max, mountainArr);
+        // System.out.println(peak);
+        if (mountainArr.get(peak) < target) return -1;
+
+        // 再从[0, peak]中找出target，找不到则[peak, max]中找
+        int result = 0;
+        result = lFindTarget(0, peak, target, mountainArr);
+        // System.out.println(result);
+        if (result != -1) return result;
+        else return rFindTarget(peak, max, target, mountainArr);
+    }
+
+    public int findPeak(int left, int right, MountainArray mountainArr) {
+        int max = right;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+
+            int m_sub = middle == 0 ? -1 : mountainArr.get(middle - 1);
+            int m_add = middle == max ? -1 : mountainArr.get(middle + 1);
+
+            int m = mountainArr.get(middle);
+            if (m > m_sub && m > m_add) {
+                return middle;
+            } else if (m_sub < m && m < m_add) {   // 上升
+                left = middle + 1;
+            } else if (m_sub > m && m > m_add) {    // 下降
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+
+    public int lFindTarget(int left, int right, int target, MountainArray mountainArr) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int m = mountainArr.get(middle);
+            if (m == target) return middle;
+            else if (m > target) {
+                right = middle - 1;
+            } else if (m < target) {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int rFindTarget(int left, int right, int target, MountainArray mountainArr) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int m = mountainArr.get(middle);
+            if (m == target) return middle;
+            else if (m > target) {
+                left = middle + 1;
+            } else if (m < target) {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
--- src/problem/hard/p25/Answer.java
+++ src/problem/hard/p25/Answer.java
@@ -1,4 +1,54 @@
 package problem.hard.p25;
 
+import common.ListNode;
+
+/**
+ * 25. K 个一组翻转链表
+ * https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+ */
 public class Answer {
+}
+
+/**
+ * @author caoPhoenix
+ * @date 2020/05/16
+ * 执行用时 :1 ms, 在所有 Java 提交中击败了57.31% 的用户
+ * 内存消耗 :40 MB, 在所有 Java 提交中击败了7.32%的用户
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode root = new ListNode(0);
+        root.next = head;
+        ListNode p = root;
+        ListNode q = head;
+        int length = listLength(head);
+        int time = length / k;
+
+        for (int i = 0; i < time; i++) {
+            int count = 0;
+            ListNode tail = q;
+            ListNode t;
+            while (count != k) {
+                t = q.next;
+                q.next = p.next;
+                p.next = q;
+                q = t;
+                count++;
+            }
+            p = tail;
+        }
+        // 倒插几次之后剩下的直接拼
+        p.next = q;
+        return root.next;
+    }
+
+    int listLength(ListNode head) {
+        int length = 0;
+        ListNode p = head;
+        while (p != null) {
+            length++;
+            p = p.next;
+        }
+        return length;
+    }
 }
