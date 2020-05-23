6c7e99924134a1ca7b3a893c130ed7f870b0531e
caoPhoenix <caophoenix@163.com>
2020-05-23 17:06:10 +0800

    today

---
+++ src/problem/hard/p76/Answer.java
@@ -0,0 +1,75 @@
+package problem.hard.p76;
+
+import java.util.*;
+
+/**
+ * 76. 最小覆盖子串
+ * https://leetcode-cn.com/problems/minimum-window-substring/
+ */
+public class Answer {
+    public static void main(String[] args) {
+        String S = "a";
+        String T = "abcv";
+        System.out.println(new Solution().minWindow(S, T));
+    }
+}
+
+
+/**
+ * @author caoPhoenix
+ * @date 2020/5/23
+ * 执行用时 :18 ms, 在所有 Java 提交中击败了54.70% 的用户
+ * 内存消耗 :40.2 MB, 在所有 Java 提交中击败了13.33%的用户
+ *
+ * 本来写了个不考虑 t 中重复字符的，用 set 来存，结果被题目坑了
+ * 然后就看了看 labuladong 大佬的题解：
+ * https://leetcode-cn.com/problems/minimum-window-substring/solution/hua-dong-chuang-kou-suan-fa-tong-yong-si-xiang-by-/
+ */
+class Solution {
+    public String minWindow(String s, String t) {
+        char[] chs = s.toCharArray();
+        char[] cht = t.toCharArray();
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        // 把需要的字符加入 need map 中
+        for (char c : cht) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;    // 滑动窗口的左右边界
+        int valid = 0;      // window 中满足要求的字符的数量
+        int start = 0, len = Integer.MAX_VALUE;
+
+        // 滑动窗口 [left, right)
+        while (right < chs.length) {
+            // 窗口右端右移
+            char c = chs[right++];
+
+            // 更新窗口
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                // Integer 不应直接 ==，有缓存问题
+                if (window.get(c).equals(need.get(c))) valid++;
+            }
+
+            // 满足条件时收缩左窗口
+            while (valid == need.size()) {
+                // 满足条件时计算最小长度
+                if (len > right - left) {
+                    len = right - left;
+                    start = left;
+                }
+                // 左端右移
+                char d = chs[left++];
+                if (window.containsKey(d)) {
+                    // 如果 window 内 d 出现的次数刚好和 need 的次数相等，
+                    // 由于次数需要减 1，故 valid 减 1
+                    if (window.get(d).equals(need.get(d))) valid--;
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        // 窗口右端移至最右侧时结束
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
--- src/problem/medium/p560/Answer.java
+++ src/problem/medium/p560/Answer.java
@@ -1,6 +1,15 @@
 package problem.medium.p560;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. 和为K的子数组
+ * https://leetcode-cn.com/problems/subarray-sum-equals-k/
+ */
 public class Answer {
+    public static void main(String[] args) {
+    }
 }
 
 
@@ -15,7 +24,7 @@ public class Answer {
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int result = 0;
-        // sums[i][j]为[i,j]的和
+        // sums 为 [i,j] 的和
         for (int i = 0; i < nums.length; i++) {
             int sum = nums[i];
             if (sum == k) result++;
@@ -28,13 +37,60 @@ class Solution {
 }
 
 /**
- * TODO 前缀和
+ * @author caoPhoenix
+ * @date 2020/05/22
+ * 执行用时 :578 ms, 在所有 Java 提交中击败了5.03% 的用户
+ * 内存消耗 :42.1 MB, 在所有 Java 提交中击败了7.69%的用户
+ *
+ * 用上了【前缀和】的思想，虽然还是 O(n^2)，但是里面的检索是可以用哈希表优化的。
+ * 需要注意的是，两层循环各自的起止条件
  */
 class Solution_1 {
     public int subarraySum(int[] nums, int k) {
         int result = 0;
-        // sums[i][j]为[i,j]的和
+        // pre[i] 为前 i 个数的和
+        int[] pre = new int[nums.length + 1];
+        pre[0] = 0;
+        for (int i = 1; i < pre.length; i++) {
+            pre[i] = pre[i - 1] + nums[i - 1];
+        }
+        // pre[i] - pre[j] = k ==> sum[j+1, i] = k
+        // 在 i 前面寻找 pre[j] = pre[i] - k
+        // pre 从 0 开始是因为还有 0 = pre[i] - k
+        // j = i-1 ==> pre[i] - pre[i-1] = nums[i] = k
+        for (int i = 1; i < pre.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pre[j] == pre[i] - k)
+                    result++;
+            }
+        }
+        return result;
+    }
+}
 
+/**
+ * @author caoPhoenix
+ * @date 2020/05/22
+ * 执行用时 :28 ms, 在所有 Java 提交中击败了52.96% 的用户
+ * 内存消耗 :40.1 MB, 在所有 Java 提交中击败了11.54%的用户
+ *
+ * 使用 hash 表来优化“检索满足条件的 j 的个数”这个过程
+ */
+class Solution_2 {
+    public int subarraySum(int[] nums, int k) {
+        int result = 0;
+        // key 为前 i 个数的和，value 为出现次数
+        Map<Integer, Integer> preMap = new HashMap<>();
+        int pre = 0;
+        preMap.put(pre, 1);
+        // pre 为前 i 个数的和, 从 0 开始是因为还有 0 = pre - k
+        // pre(i) - pre(j) = k
+        // 即 pre(j) = pre(i) - k 的 j 的个数
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            result += preMap.getOrDefault(pre - k, 0);
+            preMap.put(pre, preMap.getOrDefault(pre, 0) + 1);
+        }
         return result;
     }
 }
@@ -47,6 +103,7 @@ class Solution_x {
         int result = 0;
         // sums[i][j]为[i,j]的和
         int[][] sums = new int[nums.length][nums.length];
+        // 对于每个 i，在 [i+1, len) 之间寻找 j 满足和为 k
         for (int i = 0; i < nums.length; i++) {
             if ((sums[i][i] = nums[i]) == k) result++;
             for (int j = i + 1; j < nums.length; j++) {
